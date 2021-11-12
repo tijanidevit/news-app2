08a097b1d23dbdab89d429f1689a48bfc65d9d98
tijanidevit <tijanidevit@yahoo.com>
2021-11-12 11:13:58 +0100

    dbConfig

--- api/application/controllers/Docs.php
+++ api/application/controllers/Docs.php
@@ -19,7 +19,7 @@ class Docs extends CI_Controller
 		// 	echo 	'<script>
 		// 	window.location.href = "' . current_url() . '/view"
 		// 		  	</script>';
-		redirect("https://api.friconn.com/v2.1/docs/view");
+		redirect("http://fpinewsapp.herokuapp.com/api/docs/view");
 	}
 
 	public function view($controller_name = null)

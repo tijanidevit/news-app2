e5a06d0ee199174ace334039868a547d05c0a742
tijanidevit <tijanidevit@yahoo.com>
2021-11-15 00:30:42 +0100

    SetUPDB added

--- api/application/controllers/Setup.php
+++ api/application/controllers/Setup.php
@@ -196,9 +196,9 @@ class Setup extends REST_Controller
     }
 
     function ag_get(){
-        $query = 'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+        $query = '
+        SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
         START TRANSACTION;
-        SET time_zone = "+00:00";
         ';
         $this->setup_model->runQuert($query);
     }
